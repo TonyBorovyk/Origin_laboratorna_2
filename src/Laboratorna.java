3fb384f786df65665e043fda6bcfcef5c1434c86
Anton Boovyk <antonbovik@gmail.com>
2020-03-02 13:18:31 +0200

    (FirtsCommit)

---
+++ src/Laboratorna.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.Scanner;
+import java.lang.String;
+import java.util.ArrayList;
+
+public class Laboratorna {
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(new File("premier_league1.csv"));
+        String[] splitted;
+        String digits;
+        String line_first;
+    }
+}
