17469d6a9e97a02434f2a6aface05c23efc4d3bf
Anton Boovyk <antonbovik@gmail.com>
2020-03-02 20:18:21 +0200

    FixBag

--- src/Laboratorna.java
+++ src/Laboratorna.java
@@ -1,26 +1,33 @@
-import java.io.*;
 import java.util.Scanner;
 import java.lang.String;
 import java.util.ArrayList;
+import java.io.*;
 
 public class Laboratorna {
-
     public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(new File("premier_league1.csv"));
-        String[] splitted;
-        String digits;
-        String line_first;
-        line_first = sc.nextLine();
-        int k = Integer.parseInt(String.valueOf(line_first));
-        String[][] BoardOfResults = new String[k][5];
-        while (sc.hasNext()) {
-            ArrayList<Integer> arrayList = new ArrayList<>();
-            splitted = sc.nextLine().split(",|:");
-            for (int i = 1; i < splitted.length; i++) {
-                int a = Integer.parseInt(splitted[i]);
-                arrayList.add(a);
+        int count = 0;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Введіть назву папки: ");
+        String folder_1 = input.nextLine();
+        int counter = 0;
+        int l = CountOfTeam();
+        String[][] BoardOfResults = new String[l][5];
+    }
+
+    public static int CountOfTeam() throws Exception {
+        int teams = 0;
+        File folder = new File("FootballTeam");
+        File[] listOfFiles = folder.listFiles();
+        for (File file : listOfFiles) {
+            if (file.isFile()) {
+                Scanner sc = new Scanner(new File(file.getName()));
+                String[] splitted;
+                String line_first;
+                line_first = sc.nextLine();
+                int k = Integer.parseInt(String.valueOf(line_first));
+                teams += k;
 
             }
-        }
+        }return teams;
     }
 }
