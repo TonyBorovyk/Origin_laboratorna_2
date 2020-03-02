901ec6bac0f19360247ad262d0bba8221f38af31
MykolaOnyshchuk <54044502+MykolaOnyshchuk@users.noreply.github.com>
2020-03-02 22:47:32 +0200

    Tenth commit

--- src/Laboratorna.java
+++ src/Laboratorna.java
@@ -6,11 +6,13 @@ import java.util.ArrayList;
 public class Laboratorna {
 
     public static void main(String[] args) throws Exception {
+        int count = 0;
         Scanner sc = new Scanner(new File("premier_league1.csv"));
         String[] splitted;
         String digits;
         String line_first;
         line_first = sc.nextLine();
+        int counter = 0;
         int k = Integer.parseInt(String.valueOf(line_first));
         String[][] BoardOfResults = new String[k][5];
         while (sc.hasNext()) {
@@ -21,6 +23,35 @@ public class Laboratorna {
                 arrayList.add(a);
 
             }
+            int points = 0;
+            int wins = 0;
+            int loses = 0;
+            int draw = 0;
+            for (int j = 0; j < arrayList.size(); j = j + 2) {
+                if (arrayList.get(j) > arrayList.get(j + 1)) {
+                    points += 3;
+                    wins += 1;
+                } else if (arrayList.get(j) < (arrayList.get(j + 1))) {
+                    loses += 1;
+                } else {
+                    points += 1;
+                    draw += 1;
+                }
+            }
+            BoardOfResults[count][0] = splitted[0];
+            BoardOfResults[count][1] = Integer.toString(points);
+            BoardOfResults[count][2] = Integer.toString(wins);
+            BoardOfResults[count][3] = Integer.toString(draw);
+            BoardOfResults[count][4] = Integer.toString(loses);
+
+            count++;
+
+
         }
+        counter++;
     }
+
+
+
+
 }
