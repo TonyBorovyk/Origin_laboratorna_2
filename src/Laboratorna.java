248e80fc068bd10925260bfbd944a2a23c607658
Anton Boovyk <antonbovik@gmail.com>
2020-03-02 23:05:09 +0200

    Fixed

--- src/Laboratorna.java
+++ src/Laboratorna.java
@@ -3,7 +3,7 @@ import java.lang.String;
 import java.util.ArrayList;
 import java.io.*;
 
-public class Laboratorna {
+public class Laboratorna{
     public static void main(String[] args) throws Exception {
         int count = 0;
         Scanner input = new Scanner(System.in);
@@ -24,8 +24,47 @@ public class Laboratorna {
                 line_first = sc.nextLine();
                 int k = Integer.parseInt(String.valueOf(line_first));
 
-            }}}
+                while (sc.hasNext()) {
+                    ArrayList<Integer> arrayList = new ArrayList<>();
+                    splitted = sc.nextLine().split(",|:");
+                    for (int i = 1; i < splitted.length; i++) {
+                        int a = Integer.parseInt(splitted[i]);
+                        arrayList.add(a);
+                    }
+                    int points = 0;
+                    int wins = 0;
+                    int loses = 0;
+                    int draw = 0;
+                    for (int j = 0; j < arrayList.size(); j = j + 2) {
+                        if (arrayList.get(j) > arrayList.get(j + 1)) {
+                            points += 3;
+                            wins += 1;
+                        } else if (arrayList.get(j) < (arrayList.get(j + 1))) {
+                            loses += 1;
+                        } else {
+                            points += 1;
+                            draw += 1;
+                        }
+                    }
+                    BoardOfResults[count][0] = splitted[0];
+                    BoardOfResults[count][1] = Integer.toString(points);
+                    BoardOfResults[count][2] = Integer.toString(wins);
+                    BoardOfResults[count][3] = Integer.toString(draw);
+                    BoardOfResults[count][4] = Integer.toString(loses);
 
+                    count++;
+
+
+                }
+                counter++;
+            }
+
+
+
+
+        }
+
+    }
     public static int CountOfTeam() throws Exception {
         int teams = 0;
         File folder = new File("FootballTeam");
@@ -40,6 +79,7 @@ public class Laboratorna {
                 teams += k;
 
             }
-        }return teams;
+        }
+        return teams;
     }
 }
